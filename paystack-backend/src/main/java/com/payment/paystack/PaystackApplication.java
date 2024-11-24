8034a0f13763b33443c20de647b3b51d75ec58cd
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-24 13:44:58 +0300

    Payments with paystck integration

---
+++ paystack-backend/src/main/java/com/payment/paystack/PaystackApplication.java
@@ -0,0 +1,13 @@
+package com.payment.paystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystackApplication.class, args);
+	}
+
+}
