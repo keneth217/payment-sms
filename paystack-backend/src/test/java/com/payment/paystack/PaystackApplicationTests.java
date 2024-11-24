c0e30a781a7e0244b26839bae45fb2fe9dfc43bb
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-24 13:45:07 +0300

    Payments with paystck integration

---
+++ paystack-backend/src/test/java/com/payment/paystack/PaystackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.paystack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
