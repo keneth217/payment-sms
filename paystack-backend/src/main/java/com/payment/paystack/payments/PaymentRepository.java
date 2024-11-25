efdc505ba22f30d402a0dca310f7c10079311717
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 18:13:28 +0300

    Aadd cors,exceptions,

--- paystack-backend/src/main/java/com/payment/paystack/payments/PaymentRepository.java
+++ paystack-backend/src/main/java/com/payment/paystack/payments/PaymentRepository.java
@@ -1,7 +1,13 @@
 package com.payment.paystack.payments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
     Payment findByReference(String reference);
+
+
+    @Query("SELECT t.status FROM Payment t WHERE t.reference = :reference")
+    String findStatusByReference(@Param("reference") String reference);
 }
