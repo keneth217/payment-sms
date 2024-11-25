be110f2a7491a6e088536eda22fe319bd8c6143f
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 10:40:30 +0300

    Add paystack to angular,integrate twilio sms

--- payment-sms-frontend/src/app/app.config.ts
+++ payment-sms-frontend/src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { heroUsers,heroChevronDown,heroChevronUp, heroArrowRightCircle, heroBars4, heroDocument,
+  heroSquares2x2, heroPlusCircle, heroPercentBadge,heroWrenchScrewdriver,heroChartBar, heroCurrencyDollar,heroChatBubbleOvalLeft, heroXMark } from '@ng-icons/heroicons/outline';
 
+import { heroPlaySolid } from '@ng-icons/heroicons/solid';
 import { routes } from './app.routes';
+import { provideIcons } from '@ng-icons/core';
+import { provideHttpClient } from '@angular/common/http';
+import { Chart, registerables } from 'chart.js';
+import { AngularToastifyModule, ToastService } from 'angular-toastify';
+// Register Chart.js components
+Chart.register(...registerables);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(), // Configure HTTP Client
+    provideIcons({ heroUsers, heroArrowRightCircle, heroPlaySolid,heroWrenchScrewdriver,heroChartBar,heroChatBubbleOvalLeft, heroBars4,heroChevronDown,heroChevronUp, heroDocument, heroSquares2x2, heroPlusCircle, heroPercentBadge, heroCurrencyDollar, heroXMark }),
+    AngularToastifyModule, // Include AngularToastifyModule
+    ToastService // Provide Toast Service
+  ]
 };
