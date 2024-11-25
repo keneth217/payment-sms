49f25c47b76da65ff288d1dbf3200f0f1855073a
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-25 10:40:45 +0300

    Add paystack to angular,integrate twilio sms

--- payment-sms-frontend/src/app/app.routes.ts
+++ payment-sms-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {PaymentSuccessComponent} from "./payment-success/payment-success.component";
+import {CallbackComponent} from "./callback/callback.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'success',component:PaymentSuccessComponent},
+  {path:'payment/callback',component:CallbackComponent}
+];
